3704c988e90f3bd7e3c3aca5740779fb10a951a2
jgbf <kemuaat@gmail.com>
2022-06-08 17:37:46 +0200

    fix

--- decompress.py
+++ decompress.py
@@ -6,4 +6,4 @@ for file_name in os.listdir('dataverse_files'):
     if '.bz2' in file_name:
         temp_file = pd.read_csv(f'dataverse_files/{file_name}')
         new_file_name = file_name.replace('.bz2', '')
-        temp_file.to_csv('decompressed/file_name')
+        temp_file.to_csv(f'decompressed/{new_file_name}')
