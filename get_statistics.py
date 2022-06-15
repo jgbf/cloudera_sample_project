5ffea22b2ed8e8e3ef1c9c7a513fac1f493d42a2
jgbf <kemuaat@gmail.com>
2022-06-15 17:45:57 +0200

    add first version of statistics

---
+++ get_statistics.py
@@ -0,0 +1,26 @@
+import os
+from pyspark.sql.functions import desc
+from pyspark.sql.functions import col
+
+def read_files(folder: str):
+    return spark.read.option('header', 'true').csv(folder)
+
+def main():
+    data = read_files('/nifi/decompresed')
+
+    # Get the most departued airport
+    top_dep = data.groupby('Origin').count().sort(desc('count'))
+    top_dep.write.option('header', 'true').csv('/nifi/top_departured_airports.csv')
+
+    # Get total number of fligths per carrier
+    carrier_flight = data.groupby('UniqueCarrier').count().sort(desc('count'))
+    carrier_flight.write.option('header', 'true').csv('/nifi/carrier_count.csv')
+
+    # Calculate the most dealyed carriers
+    data = data.withColumn('delay', col('CarrierDelay').cast('int'))
+    delays = data.groupby('UniqueCarrier').sum('delay').sort(desc('sum(delay)'))
+    delays.write.option('header', 'true').csv('/nifi/delays.csv')
+
+
+if __name__ == '__main__':
+    main()
