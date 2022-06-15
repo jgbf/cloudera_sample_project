99fe6544a9e412a43e255b1e34100213fd771447
jgbf <kemuaat@gmail.com>
2022-06-15 16:28:44 +0200

    add decompress fix

--- dec_fix.py
+++ dec_fix.py
@@ -5,6 +5,9 @@ import os
 for file_name in os.listdir('dataverse_files'):
     if '.bz2' in file_name:
         if file_name in ['1995.csv.bz2', '1994.csv.bz2', '2000.csv.bz2', '2001.csv.bz2', '2002.csv.bz2', '2008.csv.bz2']:
-            temp_file = pd.read_csv(f'dataverse_files/{file_name}')
-            new_file_name = file_name.replace('.bz2', '')
-            temp_file.to_csv(f'decompressed/{new_file_name}')
+            try:
+                temp_file = pd.read_csv(f'dataverse_files/{file_name}')
+                new_file_name = file_name.replace('.bz2', '')
+                temp_file.to_csv(f'decompressed/{new_file_name}')
+            except Exception as e:
+                print(e, file_name)
